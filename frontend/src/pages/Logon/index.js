d0056428be62a6fb9421d805d89881e74d3fb90e
Phelipe <Phomena_2016@outlook.com>
2021-10-14 22:47:36 -0300

    web-application finished

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,48 +1,55 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiLogIn } from 'react-icons/fi'
-import './styles.css';
+import { FiLogIn } from 'react-icons/fi';
+import alertify from 'alertifyjs';
+
 import api from '../../services/api';
+
+import './styles.css';
+import './alertify.min.css';
+
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
-export default function Logon() {
+export default function Logon(){
     const [id, setId] = useState('');
     const history = useHistory();
-
-    async function handleLogin(e) {
+    async function handleLogin(e){
         e.preventDefault();
 
         try {
+
             const response = await api.post('sessions', { id });
             localStorage.setItem('ongId', id);
             localStorage.setItem('ongName', response.data.name);
             history.push('/profile');
         } catch (err) {
-            alert('Id não encontrado, tente novamente');
+          alertify.error('ID errado, tente novamente!');
         }
+
     }
 
     return (
         <div className="logon-container">
             <section className="form">
-               <img  src={logoImg} alt="Be the hero" />
-
+                <img src={logoImg} alt="Be The Hero" />
                 <form onSubmit={handleLogin}>
-                    <h1>Faça seu logon</h1>
+                    <h1>Logon</h1>
                     <input 
                         placeholder="Sua ID"
-                        value={id} 
+                        value={id}
                         onChange={e => setId(e.target.value)}
                     />
                     <button className="button" type="submit">Entrar</button>
+
                     <Link className="back-link" to="/register">
-                        <FiLogIn size={16} color="#E02041"/>
-                        Não tenho Cadastro
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
                     </Link>
                 </form>
+
             </section>
-            <img src={heroesImg} alt="Heroes" />
+            <img src={heroesImg} alt="Heroes"/>
         </div>
     );
 }
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
-import logoImg from '../../assets/logo.svg';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi'
-import './styles.css'
+import { FiArrowLeft } from 'react-icons/fi';
+
 import api from '../../services/api';
 
-export default function NewIncident() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [value, setValue] = useState('');
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+
+export default function NewIncident(){
+    const [title,setTitle] = useState('');
+    const [description,setDescription] = useState('');
+    const [value,setValue] = useState('');
+    const ongId = localStorage.getItem('ongId');
     const history = useHistory();
 
-    const ongId = localStorage.getItem('ongfId');
-    
-    async function HandleNewIncident(e) {
+    async function handleNewIncident(e){
         e.preventDefault();
 
         const data = {
@@ -23,60 +24,53 @@ export default function NewIncident() {
         };
 
         try {
-            await api.post('incidents', data, {
+            await api.post('incidents',data,{
                 headers: {
-                    Authorization: ongId,
+                    Authorization: ongId
                 }
-            })
+            });
 
             history.push('/profile');
+
         } catch (err) {
-            alert('Erro ao cadastrar caso, tente novamente!')
+            alert('Ocorreu um erro ao cadastrar o caso');
         }
     }
 
-    return(
+    return (
         <div className="new-incident-container">
-        <div className="content">
-            <section>
-                <img src={logoImg} alt="Be the hero" />
-                <h1>Cadastrar Novo Caso</h1>
-                <p>Descreva o caso 
-                    detalhadamente 
-                    para encontrar 
-                    um herói para 
-                    resolver isso.
-                </p>
-                <Link className="back-link" to="/profile">
-                    <FiArrowLeft size={16} color="#E02041"/>
-                    Voltar para home
-                </Link>
-            </section>
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be the Hero"/>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#E02041" />
+                        Voltar para Home
+                    </Link>
+                </section>
 
-            <form onSubmit={HandleNewIncident}>
-                <input 
-                    placeholder="Título do caso"
-                    value={title}
-                    onChange={e => setTitle(e.target.value)}
-                    
-                />
-                <textarea 
-                    placeholder="Descrição"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                    
-                />
-                <input 
-                    placeholder="Valor em reais"
-                    value={value}
-                    onChange={e => setValue(e.target.value)}
-                    
-                />
-                <button className="button" type="submit">
-                    Cadastrar
-                </button>
-            </form>
+                <form>
+                    <input 
+                        placeholder="Titulo do caso" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeholder="Descrição" 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <input 
+                        placeholder="Valor em reais" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                    />
+
+                    <button onClick={handleNewIncident} className="button" type="submit">Cadastrar</button>
+
+                </form>
+            </div>
         </div>
-    </div>
     );
 }
